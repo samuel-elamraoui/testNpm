54892be1324e21736fc705d87a387e7746ca8cb0
Samuel Elamraoui <samuel.elamraoui@Digital-Grenoble.local>
2019-04-25 16:55:40 +0200

    install imagemin plugin

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 
 (async () => {
-    await imagemin(['images/*.png'], 'build/images', {
+    await imagemin(['image/*.png'], 'build/images', {
         plugins: [
-            imageminPngquant()
+            imageminPngquant({quality: [0,0.05]})
         ]
     });
 
